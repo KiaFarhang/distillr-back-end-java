643f82301ef3b9f572cae5f068370d25c367dbbf
Kia Farhang <kfarhang0@gmail.com>
2018-03-24 17:40:57 -0500

    Start work on code to pin gthe Yelp API

--- src/main/java/com/kiafarhang/distillr/yelp/YelpAPIRequest.java
+++ src/main/java/com/kiafarhang/distillr/yelp/YelpAPIRequest.java
@@ -1,7 +1,12 @@
 package com.kiafarhang.distillr.yelp;
 
+import com.kiafarhang.distillr.yelp.YelpAPIQuery;
+import com.kiafarhang.distillr.Location;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 import org.apache.http.HttpEntity;
@@ -10,14 +15,34 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.apache.http.message.BasicHeader;
 
 public class YelpAPIRequest {
     public static String fetchYelpData() throws IOException {
 
+        Location location = new Location(29.537928, -98.521866);
+        String yelpEndpoint = YelpAPIQuery.buildYelpQueryString("hamburgers", location);
+
+        // Get API key
+
+        File apiKeyFile = new File("yelp-key.txt");
+        Scanner fileScanner = new Scanner(apiKeyFile, "UTF-8");
+        String yelpAPIKey = fileScanner.nextLine();
+        // try {
+        //     Scanner fileScanner = new Scanner(apiKeyFile, "UTF-8");
+        //     String yelpAPIKey = fileScanner.nextLine();
+        // } catch (FileNotFoundException exception) {
+        //     System.out.println(
+        //             "Error: No API key found; be sure to create 'yelp-key.txt' at the project's root directory");
+        // }
+
+        BasicHeader header = new BasicHeader("Authorization", "Bearer " + yelpAPIKey);
+
         // Set up and execute the HTTP request
 
         CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet("https://httpbin.org/get");
+        HttpGet httpGet = new HttpGet(yelpEndpoint);
+        httpGet.setHeader(header);
         CloseableHttpResponse response = httpClient.execute(httpGet);
 
         // Convert the response into a string
