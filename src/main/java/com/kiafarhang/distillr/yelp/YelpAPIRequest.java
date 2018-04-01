9be1a774c37e7c4d6d61224b51cea2a2f6b430d8
Kia Farhang <kfarhang0@gmail.com>
2018-04-01 16:11:59 -0500

    Add an overarching fetchTrips method to YelpAPIRequest class that does the main API work

--- src/main/java/com/kiafarhang/distillr/yelp/YelpAPIRequest.java
+++ src/main/java/com/kiafarhang/distillr/yelp/YelpAPIRequest.java
@@ -2,6 +2,11 @@ package com.kiafarhang.distillr.yelp;
 
 import com.kiafarhang.distillr.yelp.YelpAPIQuery;
 import com.kiafarhang.distillr.yelp.YelpBusiness;
+import com.kiafarhang.distillr.Business;
+import com.kiafarhang.distillr.Trip;
+import com.kiafarhang.distillr.yelp.YelpUtilities;
+import com.kiafarhang.distillr.yelp.HasPriceAndDistance;
+import com.kiafarhang.distillr.yelp.YelpBusinessLocation;
 
 import com.kiafarhang.distillr.server.UserRequest;
 
@@ -10,8 +15,12 @@ import com.google.gson.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
+// import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -35,13 +44,6 @@ public class YelpAPIRequest {
         File apiKeyFile = new File("yelp-key.txt");
         Scanner fileScanner = new Scanner(apiKeyFile, "UTF-8");
         String yelpAPIKey = fileScanner.nextLine();
-        // try {
-        //     Scanner fileScanner = new Scanner(apiKeyFile, "UTF-8");
-        //     String yelpAPIKey = fileScanner.nextLine();
-        // } catch (FileNotFoundException exception) {
-        //     System.out.println(
-        //             "Error: No API key found; be sure to create 'yelp-key.txt' at the project's root directory");
-        // }
 
         BasicHeader header = new BasicHeader("Authorization", "Bearer " + yelpAPIKey);
 
@@ -72,6 +74,45 @@ public class YelpAPIRequest {
         YelpAPIResponse apiResponse = gson.fromJson(content, YelpAPIResponse.class);
 
         return apiResponse.businesses;
+
+    }
+
+    public static List<Trip> fetchTrips(UserRequest request) throws IOException {
+        List<YelpBusiness> yelpBusinesses = Arrays.asList(fetchYelpData(request));
+        List<YelpBusiness> expensiveAndClosedRemoved = YelpUtilities
+                .removeExpensiveBusinesses((YelpUtilities.removeClosedBusinesses(yelpBusinesses)), request.getMoney());
+
+        @SuppressWarnings("unchecked")
+        List<YelpBusiness> sortedByPriceAndDistance = (List<YelpBusiness>) YelpUtilities
+                .sortByPriceAndDistance(expensiveAndClosedRemoved);
+
+        List<Trip> trips = new ArrayList<Trip>();
+
+        for (YelpBusiness yBusiness : sortedByPriceAndDistance) {
+            // Build the business from YelpBusiness properties
+
+            YelpBusinessLocation businessLocation = yBusiness.getLocation();
+            String address = businessLocation.getAddress1() + " " + businessLocation.getCity() + ", "
+                    + businessLocation.getZipCode();
+            Business business = new Business(yBusiness.getName(), yBusiness.getImage_url(), yBusiness.isClosed(),
+                    yBusiness.getPrice(), yBusiness.getPhone(), address, yBusiness.getUrl(), yBusiness.getCoordinates(),
+                    yBusiness.getRating());
+
+            // Use our utility functions to calculate the number of minutes and String price range
+
+            int minutes = YelpUtilities.calculateTimeNeeded(yBusiness.getCategories().get(0).getAlias(),
+                    business.getCost());
+            String cost = YelpUtilities.generateRangeFromDollarSigns(business.getCost());
+
+            // Create the Trip
+
+            Trip trip = new Trip(business, minutes, cost);
+
+            trips.add(trip);
+
+        }
+
+        return trips;
 
     }
 }
